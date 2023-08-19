b65465d3335c53fd78dff358840ad45c47fc8bd9
mandara42 <jeje4247@yahoo.fr>
2023-08-19 20:08:21 +0200

    appel api

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 const APIKey = "41bb90ba8ab8eb00f65ca309c065b1e7";
+let iconApiRes = "10d";
+
+let call_Icone = `https://openweathermap.org/img/wn/${iconApiRes}@3x.png`;
 
 let api_Call = function (city) {
 	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&lang=fr&units=metric`;
@@ -6,14 +9,55 @@ let api_Call = function (city) {
 	fetch(apiUrl)
 		.then((response) =>
 			response.json().then((data) => {
+				let date = new Date().toLocaleString("fr-FR", {
+					weekday: "long",
+					year: "numeric",
+					month: "long",
+					day: "numeric",
+					hour: "numeric",
+					minute: "numeric",
+				});
+
 				document.querySelector("#city").innerHTML = data.name;
 				document.querySelector("#temp").innerHTML = data.main.temp + "°C";
 				document.querySelector("#humidity").innerHTML =
 					data.main.humidity + "%";
 				document.querySelector("#wind").innerHTML = data.wind.speed + "km/h";
+				iconApiRes = data.weather[0].icon;
+				call_Icone = `https://openweathermap.org/img/wn/${iconApiRes}.png`;
+				document.querySelector(
+					"#icone_Meteo"
+				).innerHTML = `<img src="${call_Icone}" alt="icone météo">`;
+				document.querySelector("#date").innerHTML = `${date}`;
 			})
 		)
-		.catch((error) => console.log("erreur " + error));
+		.catch((error) => {
+			console.log("erreur " + error);
+			fetch(
+				"https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=41bb90ba8ab8eb00f65ca309c065b1e7&lang=fr&units=metric"
+			)
+				.then((response) =>
+					response.json().then((data) => {
+						let date = new Date().toLocaleString("fr-FR", {
+							weekday: "long",
+							year: "numeric",
+							month: "long",
+							day: "numeric",
+							hour: "numeric",
+							minute: "numeric",
+						});
+						document.querySelector("#city").innerHTML = data.name;
+						document.querySelector("#temp").innerHTML = data.main.temp + "°C";
+						document.querySelector("#humidity").innerHTML =
+							data.main.humidity + "%";
+						document.querySelector("#wind").innerHTML =
+							data.wind.speed + "km/h";
+
+						document.querySelector("#date").innerHTML = `${date}`;
+					})
+				)
+				.catch((error) => console.log("erreur " + error));
+		});
 };
 
 document.querySelector("form").addEventListener("submit", function (event) {
